f1c2daca72a8b9c333ced14c34cfd32640c14f78
Pankaj Suthar <sutharpan6004@gmail.com>
2023-06-27 13:53:53 +0530

    dummy file

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <Link href={"/users"}>users</Link>
+    </>
+  )
+}
---
+++ src/app/users/page.jsx
@@ -0,0 +1,28 @@
+function Page({data}) {
+    console.log(data)
+    return (
+      <div>
+      {
+        data.map(elem=>{
+          return(
+            <p key={elem.id}>{elem.title}</p>
+          )
+        })
+      }
+      </div>
+    )
+  }
+  
+  export async function getStaticProps(){
+      let response=await fetch("https://jsonplaceholder.typicode.com/users")
+      let data=await response.json()
+      console.log(data)
+      return {
+          props:{
+              data
+          }
+      }
+  }
+  
+  export default Page
+  
